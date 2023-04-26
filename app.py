d273821a2ac148091a43aeb3eeeeb74b59288534
memo124 <48780097+memo124@users.noreply.github.com>
2023-04-26 14:18:01 -0600

    Adicion de metodo DELETE

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import json
 from datetime import date
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html
+
 # Declaración de modelo basandose a al base de datos
 class estudiante(BaseModel):
     id: int | None = None
@@ -21,9 +24,18 @@ conect = mysql.connector.connect(
 )
 
 app = FastAPI()
+
+@app.get("/openapi.json")
+async def get_open_api_endpoint():
+    return JSONResponse(get_openapi(title="Tu API", version="0.0.2", routes=app.routes))
+
+@app.get("/docs2", include_in_schema=False)
+async def get_documentation():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="documentación")
+
 # Endpoint para la obtención de todos los estudiantes
 @app.get("/estudiante")
-def getEstudiantes():
+async def getEstudiantes():
     cursor = conect.cursor()
     query="select * from estudiante;"
     cursor.execute(query)
@@ -41,7 +53,7 @@ def getEstudiantes():
     return JSONResponse(content=response)
 # Endpoint para la obtención de un solo estudiante
 @app.get("/estudiante/{id}")
-def getEstudiante(id: int):
+async def getEstudiante(id: int):
     cursor = conect.cursor()
     query="select * from estudiante where id = %s;"
     cursor.execute(query,(id,))
@@ -60,7 +72,7 @@ def getEstudiante(id: int):
     return JSONResponse(content=response)
 # Endpoint para actualizar datos de un estudiante
 @app.put("/estudiante/{id}")
-def putEstudiante(id: int, body :estudiante):
+async def putEstudiante(id: int, body :estudiante):
     cursor = conect.cursor()
     response = { **body.dict() }
     response.update({"id":id})
@@ -72,7 +84,7 @@ def putEstudiante(id: int, body :estudiante):
     return JSONResponse(content={"mensaje": f"El usuario con id {id} ha sido actualizado"})
 # Endpoint para el ingreso de un estudiante
 @app.post("/estudiante")
-def postEstudiante(body:estudiante):
+async def postEstudiante(body:estudiante):
     cursor = conect.cursor()
     today = date.today()   
     sql = "INSERT INTO estudiante(nombre,apellido,carnet,creacion) VALUES(%s,%s,%s,%s)"
@@ -81,3 +93,12 @@ def postEstudiante(body:estudiante):
     conect.commit()
     cursor.close()
     return JSONResponse(content={"mensaje": f"El usuario ha sido agregado"})
+
+@app.delete("/estudiante/{id}")
+async def deteleEstudiante(id : int):  
+    cursor = conect.cursor()
+    sql = "delete from estudiante where id = %s;"
+    cursor.execute(sql,(id,))
+    conect.commit()
+    cursor.close()
+    return JSONResponse(content={"mensaje": f"El usuario ha sido eliminado"})
